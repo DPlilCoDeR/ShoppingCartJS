f6051b18c79d4cbd645b1528f992db24686771ab
doblepapi <jasonbnick@gmial.com>
2020-08-05 00:46:31 +0200

    course in shopping cart

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const listaCursos = document.querySelector('#lista-cursos');
 const carrito = document.querySelector('#carrito')
+const listaCarrito = document.querySelector('#lista-carrito tbody')
 
 ejecutaEventListener();
 
 function ejecutaEventListener() {
-    listaCursos.addEventListener('click', cargarCarrito);
+    listaCursos.addEventListener('click', seleccionarCurso);
 }
 
-function cargarCarrito(event) {
+function seleccionarCurso(event) {
     event.preventDefault();
     if (event.target.classList.contains('agregar-carrito')) {
         let curso = event.target.parentElement.parentElement;
@@ -19,7 +20,23 @@ function leeDatosCurso(curso) {
     const dataCurso = {
         imagen: curso.querySelector('img').src,
         nombre: curso.querySelector('h4').innerText,
-        precio: curso.querySelector('.precio span').innerText
+        precio: curso.querySelector('.precio span').innerText,
+        id: curso.querySelector('a').getAttribute('data-id')
     }
-    console.log(dataCurso)
+    creaFilaDataCursoCarrito(dataCurso);
+}
+
+function creaFilaDataCursoCarrito(curso) {
+    const filaCurso = document.createElement('tr');
+    filaCurso.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width="100px">
+        </td>
+        <td>${curso.nombre}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href='#' class='borrar-curso' data-id=${curso.id} >X</a>
+        </td>
+    `;
+    listaCarrito.append(filaCurso);
 }

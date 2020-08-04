a441ec3637509aed2010513682aa7dc50a0eb7b6
doblepapi <jasonbnick@gmial.com>
2020-08-04 21:48:30 +0200

    get course selected

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 const listaCursos = document.querySelector('#lista-cursos');
+const carrito = document.querySelector('#carrito')
 
-listaCursos.addEventListener('click', cargarCarrito);
+ejecutaEventListener();
+
+function ejecutaEventListener() {
+    listaCursos.addEventListener('click', cargarCarrito);
+}
 
 function cargarCarrito(event) {
+    event.preventDefault();
     if (event.target.classList.contains('agregar-carrito')) {
-        console.log(event.target.parentNode);
-        console.log(`Agregado al carrito ${event}`);
+        let curso = event.target.parentElement.parentElement;
+        leeDatosCurso(curso)
     }
+}
+
+function leeDatosCurso(curso) {
+    const dataCurso = {
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').innerText,
+        precio: curso.querySelector('.precio span').innerText
+    }
+    console.log(dataCurso)
 }
